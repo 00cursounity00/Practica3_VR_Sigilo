b86ff40b761d0184a9f2404b2644eaf22d440dc9
00cursounity00 <00cursounity00@gmail.com>
2019-11-06 13:53:19 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Movedor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movedor : MonoBehaviour
+{
+    [SerializeField] Transform origen;
+    [SerializeField] Transform destino;
+    [SerializeField] float velocidad;
+    [SerializeField] int direccion;
+    private float porcentaje = 0;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();    
+    }
+
+    void Update()
+    {
+        porcentaje += Time.deltaTime * velocidad * direccion;
+        transform.position = Vector3.Lerp(origen.position, destino.position, porcentaje);
+        if (porcentaje >= 1 || porcentaje <= 0)
+        {
+            direccion *= -1;
+            //transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * direccion, transform.localScale.y);
+            //transform.localScale = new Vector2(direccion, 1); Solo si la escala es 1/1/1
+            porcentaje = Mathf.Clamp(porcentaje, 0, 1f);
+        }
+    }
+}
